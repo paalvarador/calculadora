c11411af26dd954488e53bbad823e26461553740
paalvarador <paavlvarador@gmail.com>
2023-06-05 23:28:38 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# calculadora
+
+Este es un proyecto de una calculadora básica desarrollada con HTML, CSS y Vanilla JavaScript
+Se desarrollad para repasar habilidades en el desarrollo de proyectos Web.
+
---
+++ scripts.js
@@ -0,0 +1,21 @@
+let displayValue = '';
+
+function appendValue(value) {
+    displayValue += value;
+    document.getElementById('display').value = displayValue;
+}
+
+function calculate() {
+    let result = '';
+    try {
+        result = eval(displayValue);
+    } catch (error) {
+        result = 'Error';
+    }
+    document.getElementById('display').value = result;
+}
+
+function clearDisplay() {
+    displayValue = '';
+    document.getElementById('display').value = '';
+}
